391f5b21e07012984a3ea1a337545ba315b33276
Miguel de Alba <miguelalba89@gmail.com>
2021-06-28 19:23:20 +0200

    Replace full metadata with metadata views
    Replace full metadata with metadata views from the backend with just the needed information.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,46 +24,13 @@ class ModelTable extends React.Component {
     }
 
     renderModel(model, index) {
-
-        let modelType = "fsk:modelType" in model ? model["fsk:modelType"] : "";
-        let name = "";
-        let software = "";
-        let products = [];
-        let hazards = [];
-
-        if ("fsk:generalInformation" in model) {
-            let generalInformation = model['fsk:generalInformation'];
-
-            if ('fsk:name' in generalInformation) {
-                name = generalInformation['fsk:name'];
-            }
-
-            if ('fsk:languageWrittenIn' in generalInformation) {
-                software = generalInformation['fsk:languageWrittenIn'];
-            }
-        }
-
-        if ("fsk:scope" in model) {
-            let scope = model["fsk:scope"];
-
-            // Get products
-            if ('fsk:product' in scope) {
-                products = scope['fsk:product'].map(it => it['fsk:name']);
-            }
-
-            // Get hazards
-            if ('fsk:hazard' in scope) {
-                hazards = scope['fsk:hazard'].map(it => it['fsk:name']);
-            }
-        }
-
         return (
             <tr key={index}>
-                <td>{name}</td>
-                <td>{products}</td>
-                <td>{hazards}</td>
-                <td>{software}</td>
-                <td>{modelType}</td>
+                <td>{model["name"]}</td>
+                <td>{model["products"]}</td>
+                <td>{model["hazards"]}</td>
+                <td>{model["software"]}</td>
+                <td>{model["type"]}</td>
             </tr>
         );
     }
@@ -77,7 +44,7 @@ class Home extends React.Component {
             metadata: null
         };
 
-        fetch("http://localhost:8000/models")
+        fetch("http://localhost:8000/simplemodels")
             .then(response => response.json())
             .then(data => this.setState({ metadata: data }));
     }
